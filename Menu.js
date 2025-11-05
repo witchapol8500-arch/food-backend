7a1a41f705ef5c4a389a248987bdbfc199d73a60
witchapol8500-arch <witchapol8500@gmail.com>
2025-11-05 14:55:08 +0700

    Add files via upload

--- Menu.js
+++ Menu.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// นี่คือ "พิมพ์เขียว" ของเมนูอาหาร
+// นี่คือ \"พิมพ์เขียว\" ของเมนู (อัปเกรด)
 const menuSchema = new Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    image: { type: String, required: true }
+    image: { type: String, required: false },
+    
+    // --- (นี่คือช่องใหม่ที่เราเพิ่ม) ---
+    category: { type: String, required: true, default: 'อาหารจานเดียว' } 
+    // default: 'อาหารจานเดียว' หมายถึง ถ้าเราไม่กรอก มันจะใส่หมวดนี้ให้
 });
 
+// สร้างโมเดลชื่อ 'Menu' จากพิมพ์เขียวนี้
 module.exports = mongoose.model('Menu', menuSchema);
